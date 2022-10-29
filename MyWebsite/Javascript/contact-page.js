aff743de5bf984c8786b7e49bce198e4d0f9301d
Roman Kus <101915809+Roman-Kus@users.noreply.github.com>
2022-10-29 18:45:42 -0500

    Add files via upload

---
+++ MyWebsite/Javascript/contact-page.js
@@ -0,0 +1,120 @@
+let nameError = document.querySelector('#Name-Error');
+let emailError = document.querySelector('#Email-Error');
+let subjectError = document.querySelector('#Subject-Error');
+let messageError = document.querySelector('#Message-Error');
+let submitError = document.querySelector('#Submit-Error');
+
+function validateName() {
+
+  let name = document.getElementById('Full-Name').value;
+
+  if(name.length == 0){
+    nameError.textContent = 'Name is required';
+    return false;
+  }
+
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.textContent = 'Write full name';
+    return false;
+  }
+
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateEmail() {
+
+  let email = document.getElementById('Email').value;
+
+  if(email.length == 0){
+    emailError.textContent = 'Email is required';
+    return false;
+  }
+
+  if(!(email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))){
+    emailError.textContent = 'Invalid Email';
+    return false;
+  }
+
+  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateSubject() {
+
+  let subject = document.getElementById('Subject').value;
+  const required = 3;
+  let remainder = required - subject.length;
+
+  if(remainder > 0) {
+    subjectError.textContent = remainder + ' more characters required';
+    return false;
+  }
+
+  subjectError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateMessage() {
+
+  let message = document.getElementById('Message').value;
+  const required = 15;
+  let remainder = required - message.length;
+
+  if(remainder > 0) {
+    messageError.textContent = remainder + ' more characters required';
+    return false;
+  }
+
+  messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validate () {
+
+  submitError.style.display = 'block';
+  submitError.style.color = 'rgb(255, 81, 81)';
+
+  if(!validateName() && !validateEmail() && !validateSubject() && !validateMessage()) {
+    submitError.textContent = 'Please input details';
+    setTimeout(function(){
+      submitError.style.display = 'none';}, 3000)
+    return false;
+  }
+
+  if(!validateName()) {
+    submitError.textContent = 'Please write in a valid name';
+    setTimeout(function(){
+      submitError.style.display = 'none';}, 3000)
+    return false;
+  }
+
+  if(!validateEmail()) {
+    submitError.textContent = 'Please write in a valid email';
+    setTimeout(function(){
+      submitError.style.display = 'none';}, 3000)
+    return false;
+  }
+
+
+  if(!validateSubject()) {
+    submitError.textContent = 'Please write in a valid subject';
+    setTimeout(function(){
+      submitError.style.display = 'none';}, 3000)
+    return false;
+  }
+
+
+  if(!validateMessage()) {
+    submitError.textContent = 'Please write in a valid message';
+    setTimeout(function(){
+      submitError.style.display = 'none';}, 3000)
+    return false;
+  }
+
+  submitError.textContent = 'Thank You!';
+  submitError.style.color = '#47AB3C';
+  setTimeout(function(){
+    submitError.style.display = 'none';}, 3000);
+  return true;
+}
---
+++ MyWebsite/Javascript/home-page.js
@@ -0,0 +1,13 @@
+let screenHeight = window.clientHeight;
+
+if(screenHeight <= 700){
+  document.getElementById("Home-Page").style.height = screenHeight + 'px';
+  let homePage = document.querySelector("#Home-Page");
+  let textOnHome = document.getElementById("Text-On-Home");
+
+  homePage.style.display = 'block';
+  textOnHome.style.position = 'relative';
+  textOnHome.style.top = (screenHeight / 2) - 74.375 + 'px';
+
+  document.getElementById("Home-Page").style.height = screenHeight;
+}
